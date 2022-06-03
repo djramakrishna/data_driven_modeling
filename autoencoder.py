d20ee516cf6fb9c20d2b7f8aa8ac29667edc38e7
Rama Krishna <jnanaramakrishnad@gmail.com>
2022-06-03 00:42:17 -0700

    Update autoencoder.py

--- autoencoder.py
+++ autoencoder.py
@@ -5,10 +5,9 @@ import torch.nn.functional as F
 class myAutoEncoder(nn.Module):
     def __init__(self, classes=2):
         super(myAutoEncoder, self).__init__()    
+        
         self.encoder = nn.Sequential(
-            
             #input for the encoder : 3 28 28
-
             nn.Conv2d(3, 16, 3, stride=1, padding=1),  
             nn.Tanh(True),
             nn.MaxPool2d(2),  
@@ -16,25 +15,20 @@ class myAutoEncoder(nn.Module):
             nn.Conv2d(16, 4, 3, stride=1, padding=1), 
             nn.Tanh(True),
             nn.MaxPool2d(2)
-
             #output for encoder :  4 7 7
         )
 
-        self.decoder = nn.Sequential(
-            
+        self.decoder = nn.Sequential(       
             #Input of decoder : 4 7 7 
-
             nn.ConvTranspose2d(4, 16, 2, stride=2),
             nn.Tanh(True),
                 
             nn.ConvTranspose2d(16, 3, 2, stride=2),
             nn.Sigmoid()
-
             #output of decoder : 3 28 28
         )
 
-        self.classifier = nn.Sequential(
-            
+        self.classifier = nn.Sequential(        
             #Input of classifier : 3 28 28 
             nn.Linear(3*28*28, self.classes)
         )
