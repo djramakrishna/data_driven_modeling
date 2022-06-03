5401bb04fad6735603145c48d4d3ef0231be9c55
Rama Krishna <jnanaramakrishnad@gmail.com>
2022-06-03 00:45:06 -0700

    Create dataloader.py

---
+++ dataloader.py
@@ -0,0 +1,28 @@
+from torch.utils.data import Dataset, DataLoader
+import glob
+import cv2
+import torch
+
+class CustomDataset(Dataset):
+	def __init__(self):
+		self.dataset_path = "" 										              # Give path for the dataset
+		directory_list = glob.glob(self.dataset_path + "*") 		# This list should contain paths for both freeze and nofreeze folders(containing images)
+		self.data = []												                  # List containing list of tensor_img and class_name [[img1, class_name1],[img2, class_name2]....]
+		for single_class in directory_list:	
+			class_name = single_class.split("/")[-1]
+			for tensor_img in glob.glob(single_class + "/*.png"):	# Change the format of .png based on the image extensions 
+				self.data.append([tensor_img, class_name])
+		
+		self.class_map = {"freeze" : 0, "nofreeze": 1} 
+
+	def __len__(self):
+		return len(self.data)    
+	
+	def __getitem__(self, idx):
+		tensor_image, class_name = self.data[idx]
+		class_id = self.class_map[class_name]
+		#img_tensor = torch.from_numpy(img)		### Exclude this if they are already a torch tensor
+		class_id = torch.tensor([class_id])
+		return img_tensor, class_id
+		
+
