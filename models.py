4c80c85d626bd54343934247c308a0ebece2ad88
Rama Krishna <jnanaramakrishnad@gmail.com>
2022-06-05 15:09:08 -0700

    Update models.py

--- models.py
+++ models.py
@@ -1,40 +1,47 @@
+from re import X
 import torch.nn as nn
 import math
 import torch
 from torch.nn import functional as F
 
 class myAutoEncoder(nn.Module):
-	def __init__(self, classes=2):
+	def __init__(self):
 		super(myAutoEncoder, self).__init__()    
+
 		self.encoder = nn.Sequential(
 			#input for the encoder : 3 28 28
 			nn.Conv2d(3, 16, 3, stride=1, padding=1),  
-			nn.Tanh(True),
+			nn.ReLU(),
+			#nn.Tanh(True),
 			nn.MaxPool2d(2),
 			nn.Conv2d(16, 4, 3, stride=1, padding=1), 
-			nn.Tanh(True),
-			nn.MaxPool2d(2)
+			nn.ReLU(),
+			#nn.Tanh(True),
+			nn.MaxPool2d(2),
 			#output for encoder :  4 7 7
 		)
 
 		self.decoder = nn.Sequential(         
 			#Input of decoder : 4 7 7 
 			nn.ConvTranspose2d(4, 16, 2, stride=2),
-			nn.Tanh(True),         
+			nn.ReLU(),
+			#nn.Tanh(True),         
 			nn.ConvTranspose2d(16, 3, 2, stride=2),
-			nn.Sigmoid()
+			nn.Sigmoid(),
 			#output of decoder : 3 28 28
 		)
 		
 		self.classifier = nn.Sequential(
 			#Input of classifier : 3 28 28 3 
-			nn.Linear(3*28*28*3, 3*28*28*6),
-			nn.ReLU(inplace=True),
-			nn.Dropout(),
-			nn.Linear(3*28*28*6, 3*28*28*3),
-			nn.ReLU(inplace=True),
-			nn.Linear(3*28*28*3, 28),
-			nn.Linear(28, 2)
+			
+			nn.Linear(714096, 2),
+
+			# nn.ReLU(inplace=True),
+			# nn.Dropout(),
+			# nn.Linear(3*28*28*6, 3*28*28*3),
+			# nn.ReLU(inplace=True),
+			# nn.Linear(3*28*28*3, 28),
+			# nn.Linear(28, 2),
 		)
 
 	def forward(self, x, y, z):
@@ -51,6 +58,9 @@ class myAutoEncoder(nn.Module):
 		z = z.view(z.size(0), -1)
 
 		output = torch.cat((x, y, z), 1)
-		output = self.classifier(x)
+		print(output.shape)
+		print("ENTERED CLASSIFER")
+		output = self.classifier(output)
+		output = F.softmax(output, dim=1)
 
 		return output
