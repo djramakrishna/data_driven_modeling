695a2af93a5b5b46a1b29c9e8f07f1c22a6c8cf9
Rama Krishna <jnanaramakrishnad@gmail.com>
2022-06-05 00:05:04 -0700

    Update datasets.py

--- datasets.py
+++ datasets.py
@@ -1,17 +1,26 @@
 from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms
 import glob
 import cv2
+from torchvision.io import read_image
 import torch
+import os
+from PIL import Image
 
 class CustomDataset(Dataset):
 	def __init__(self):
-		self.dataset_path = "" 						# dataset path
-		directory_list = glob.glob(self.dataset_path + "*") 		# This list should contain paths for both freeze and nofreeze folders(containing images)
-		self.data = []						        # List containing list of tensor_img and class_name [[img1, class_name1],[img2, class_name2]....]
-		for single_class in directory_list:	
-			class_name = single_class.split("/")[-1]
-			for tensor_img in glob.glob(single_class + "/*.png"):	# Change the format of .png based on the image extensions 
-				self.data.append([tensor_img, class_name])
+		self.dataset_path = os.getcwd() + "/dataset"					                   	
+		directory_list = glob.glob(self.dataset_path + "*") 		
+		self.data = []
+		self.transform = transforms.Compose([transforms.PILToTensor])
+		for each_class in directory_list:
+			class_name = each_class.split("/")[-1]
+			persons_list = glob.glob(each_class + "/*")
+			for each_person in persons_list:
+				img_x = glob.glob(each_person + "/x.png")
+				img_y = glob.glob(each_person + "/y.png")
+				img_z = glob.glob(each_person + "/z.png")
+				self.data.append([img_x, img_y, img_z, class_name])
 		
 		self.class_map = {"freeze" : 0, "nofreeze": 1} 
 
@@ -19,17 +28,17 @@ class CustomDataset(Dataset):
 		return len(self.data)    
 	
 	def __getitem__(self, idx):
-		tensor_image, class_name = self.data[idx]
+		img_x_path, img_y_path, img_z_path, class_name = self.data[idx][0][0], self.data[idx][1][0], self.data[idx][2][0], self.data[idx][3]
+		
+		img_x = Image.open(img_x_path)
+		img_y = Image.open(img_y_path)
+		img_z = Image.open(img_z_path)
+
+		tensor_x = self.transform(img_x)
+		tensor_y = self.transform(img_y)
+		tensor_z = self.transform(img_z)
+
 		class_id = self.class_map[class_name]
-		#img_tensor = torch.from_numpy(img)		### Exclude this if they are already a torch tensor
 		class_id = torch.tensor([class_id])
-		return tensor_image, class_id
 
-
-def getDataLoader(mode, batch_size, dataset, num_workers=4):
-    
-    dataloader = DataLoader(dataset, 
-                            batch_size=batch_size, 
-                            shuffle=(mode=='train'),
-                            num_workers=num_workers)
-    return dataloader
+		return tensor_x, tensor_y, tensor_z, class_id
